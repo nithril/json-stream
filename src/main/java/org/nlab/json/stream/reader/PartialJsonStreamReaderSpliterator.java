66b21fa652e8037ebfe35213ca6c77786926ddc7
Nicolas LABROT <nicolas.labrot@victorbuckservices.com>
2016-07-20 14:09:53 +0200

    Add a new test

--- src/main/java/org/nlab/json/stream/reader/PartialJsonStreamReaderSpliterator.java
+++ src/main/java/org/nlab/json/stream/reader/PartialJsonStreamReaderSpliterator.java
@@ -1,7 +1,6 @@
 package org.nlab.json.stream.reader;
 
-import java.util.Spliterator;
-import java.util.function.Consumer;
+import static org.nlab.json.stream.context.token.ObjectToken.KEY_UNINIT;
 
 import org.nlab.exception.UncheckedExecutionException;
 import org.nlab.json.stream.context.StreamContext;
@@ -9,89 +8,93 @@ import org.nlab.json.stream.context.token.ArrayToken;
 import org.nlab.json.stream.context.token.ObjectToken;
 import org.nlab.json.stream.context.token.Token;
 
-import com.fasterxml.jackson.core.JsonToken;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 /**
  * Created by nlabrot on 08/12/15.
  */
 public class PartialJsonStreamReaderSpliterator implements Spliterator<StreamContext> {
 
-    private final JsonMatcherStreamReader jsonMatcherReader;
-    private final int depth;
-    private final Token token;
+  private final JsonMatcherStreamReader jsonMatcherReader;
+  private final int depth;
+  private final Token token;
 
-    private boolean eoi = false;
+  private boolean eoi = false;
 
-    private String key;
-    private int index;
+  private String key;
+  private int index;
 
 
-    public PartialJsonStreamReaderSpliterator(JsonMatcherStreamReader jsonMatcherReader) {
-        this.jsonMatcherReader = jsonMatcherReader;
-        this.depth = jsonMatcherReader.getStreamContext().getElements().size();
-        this.token = jsonMatcherReader.getStreamContext().getElements().peek();
+  public PartialJsonStreamReaderSpliterator(JsonMatcherStreamReader jsonMatcherReader) {
+    this.jsonMatcherReader = jsonMatcherReader;
+    this.depth = jsonMatcherReader.getStreamContext().getElements().size();
+    this.token = jsonMatcherReader.getStreamContext().getElements().peek();
 
-        if (this.token instanceof ObjectToken){
-            key = ((ObjectToken) this.token).getKey();
-        }else if (this.token instanceof ArrayToken){
-            index = ((ArrayToken) this.token).getIndex();
-        }
-
+    if (this.token instanceof ObjectToken) {
+      key = ((ObjectToken) this.token).getKey();
+    } else if (this.token instanceof ArrayToken) {
+      index = ((ArrayToken) this.token).getIndex();
     }
 
-    @Override
-    public boolean tryAdvance(Consumer<? super StreamContext> action) {
-        try {
-            if (eoi) {
-                return false;
-            }
+  }
 
-            if (jsonMatcherReader.isClosed()) {
-                return false;
-            }
+  @Override
+  public boolean tryAdvance(Consumer<? super StreamContext> action) {
+    try {
+      if (eoi) {
+        return false;
+      }
 
-            if (jsonMatcherReader.nextToken() == null) {
-                return false;
-            }
+      if (jsonMatcherReader.isClosed()) {
+        return false;
+      }
 
-            action.accept(jsonMatcherReader.getStreamContext());
+      if (jsonMatcherReader.nextToken() == null) {
+        return false;
+      }
 
-            if (jsonMatcherReader.getStreamContext().getElements().size() == depth){
+      action.accept(jsonMatcherReader.getStreamContext());
 
-                if (token instanceof ObjectToken){
-                    if (((ObjectToken) token).getKey().equals(key)){
-                        return true;
-                    }
-                }else if (token instanceof ArrayToken){
-                    if (((ArrayToken) token).getIndex() == index){
-                        return true;
-                    }
-                }
+      if (jsonMatcherReader.getStreamContext().getElements().size() == depth) {
 
-                eoi = true;
-                return false;
-            }
-
-
+        if (token instanceof ObjectToken) {
+          if (((ObjectToken) token).getKey().equals(key) || KEY_UNINIT.equals(key)) {
             return true;
-
-        } catch (Exception e) {
-            throw new UncheckedExecutionException(e);
+          }
+        } else if (token instanceof ArrayToken) {
+          if (((ArrayToken) token).getIndex() == index) {
+            return true;
+          }
         }
-    }
 
-    @Override
-    public Spliterator trySplit() {
-        return null;
-    }
+        eoi = true;
+        return false;
+      } else if (jsonMatcherReader.getStreamContext().getElements().size() < depth) {
+        eoi = true;
+        return false;
+      }
 
-    @Override
-    public long estimateSize() {
-        return 0;
-    }
 
-    @Override
-    public int characteristics() {
-        return Spliterator.NONNULL | Spliterator.DISTINCT | Spliterator.IMMUTABLE;
+      return true;
+
+    } catch (Exception e) {
+      throw new UncheckedExecutionException(e);
     }
+  }
+
+  @Override
+  public Spliterator trySplit() {
+    return null;
+  }
+
+  @Override
+  public long estimateSize() {
+    return 0;
+  }
+
+  @Override
+  public int characteristics() {
+    return Spliterator.NONNULL | Spliterator.DISTINCT | Spliterator.IMMUTABLE;
+  }
 }
